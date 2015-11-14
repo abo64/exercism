d9c459c23f0f7e92692aca450d9fbe1e757374b8
Achim <abo64@gmx.de>
2015-11-14 10:18:58 +0100

    haskell/roman-numerals: simplified solution w/ find

--- haskell/roman-numerals/Roman.hs
+++ haskell/roman-numerals/Roman.hs
@@ -1,32 +1,23 @@
 module Roman (numerals) where
 
-import Data.List (unfoldr)
+import Data.List (unfoldr, find)
 import Control.Applicative ((<$>))
 
 type Numerals = String
 type NumeralConversion = (Numerals,Int)
-type NumeralConversions = [NumeralConversion]
 
 numerals :: Int -> Numerals
-numerals int =
-  concat $ unfoldr nextNumeral int
+numerals =
+  concat <$> unfoldr nextNumeral
 
 nextNumeral :: Int -> Maybe NumeralConversion
 nextNumeral int =
-  maybeNext $ filter lteInt numeralConversions
+  subtractFound <$> find lteInt numeralConversions
   where
-    maybeNext :: NumeralConversions -> Maybe NumeralConversion
-    maybeNext ncs = diff <$> maybeHead ncs
-    diff :: NumeralConversion -> NumeralConversion
-    diff (n,i) =  (n, int - i)
-    lteInt :: NumeralConversion -> Bool
+    subtractFound (n,i) = (n, int - i)
     lteInt (_,i) = i <= int
 
-maybeHead :: [a] -> Maybe a
-maybeHead (x:_) = Just x
-maybeHead _ = Nothing
-
-numeralConversions :: NumeralConversions
+numeralConversions :: [NumeralConversion]
 numeralConversions =
  [("M", 1000), ("CM", 900), ("D", 500), ("CD", 400),
   ("C", 100),  ("XC", 90),  ("L", 50),  ("XL", 40),
