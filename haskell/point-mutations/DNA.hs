ec5d2b0eed1e4e48d14404fe58104486b3a9c27b
Achim <abo64@gmx.de>
2015-11-18 08:55:40 +0100

    haskell/point-mutations: point-free solution

--- haskell/point-mutations/DNA.hs
+++ haskell/point-mutations/DNA.hs
@@ -1,8 +1,7 @@
 module DNA (hammingDistance) where
 
 hammingDistance :: String -> String -> Int
-hammingDistance strand1 strand2 =
-  sum nucleotideDistances
-    where
-      nucleotideDistances = zipWith nucleotideDistance strand1 strand2
-      nucleotideDistance = (fromEnum .) . (/=)
+hammingDistance =
+  (sum .) . zipWith nucleotideDistance
+  where
+    nucleotideDistance = (fromEnum .) . (/=)
