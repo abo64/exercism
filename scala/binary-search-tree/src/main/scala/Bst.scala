3638417ac7940eb6470785aac396b06e2139cc87
Achim <abo64@gmx.de>
2015-11-23 07:42:31 +0100

    cleanup

--- scala/binary-search-tree/src/main/scala/Bst.scala
+++ scala/binary-search-tree/src/main/scala/Bst.scala
@@ -19,27 +19,6 @@ case class Bst[T: OrderedValues](value: T,
     val leftA = left.fold(z)(_.foldLeft(z)(f))
     val a = f(leftA, value)
     right.fold(a)(_.foldLeft(a)(f))
-//    this match {
-//      case Bst(t, None, None) => f(z,t)
-//      case Bst(t, Some(l), None) => f(l.foldLeft(z)(f),t)
-//      case Bst(t, None, Some(r)) => r.foldLeft(f(z,t))(f)
-//      case Bst(t, Some(l), Some(r)) => {
-//        val leftA = f(l.foldLeft(z)(f),t)
-//        r.foldLeft(leftA)(f)
-//      }
-//    }
-  }
-
-  def foreach[U](f: T => U): Unit = {
-    left foreach (_.foreach(f))
-    f(value)
-    right foreach (_.foreach(f))
-//    this match {
-//      case Bst(t, None, None) => f(t)
-//      case Bst(t, Some(l), None) => { l.foreach(f); f(t) }
-//      case Bst(t, None, Some(r)) => { f(t); r.foreach(f) }
-//      case Bst(t, Some(l), Some(r)) => { l.foreach(f); f(t); r.foreach(f) }
-//    }
   }
 }
 
@@ -53,11 +32,4 @@ object Bst {
 
   def toList[T](bst: Bst[T]): List[T] =
     bst.foldLeft(List.empty[T])((l,t) => l :+ t)
-
-//  def toList[T <% Ordered[T]](bst: Bst[T]): List[T] = {
-//    import scala.collection.mutable.ListBuffer
-//    val lb = ListBuffer[T]()
-//    bst foreach (lb += _)
-//    lb toList
-//  }
 }
