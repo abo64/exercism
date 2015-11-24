47eaf8c1298c16690008421bffd20cfcd0e94902
Achim <abo64@gmx.de>
2015-11-24 07:50:21 +0100

    java/meetup: solution using a Stream

--- java/meetup/src/main/java/Meetup.java
+++ java/meetup/src/main/java/Meetup.java
@@ -1,5 +1,8 @@
+import java.util.Iterator;
 import java.util.function.Function;
 import java.util.function.Predicate;
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
 
 import org.joda.time.DateTime;
 
@@ -31,27 +34,45 @@ public class Meetup {
         return null;
     }
 
-    private DateTime next(final int weekDay, DateTime startDate) {
+    private static DateTime next(final int weekDay, DateTime startDate) {
         return loopUntil(startDate.plusDays(1),
                 dt -> dt.getDayOfWeek() == weekDay, dt -> dt.plusDays(1));
-//        return loopUntil(startDate.plusDays(1), weekDay, +1);
     }
 
-    private DateTime previous(final int weekDay, DateTime startDate) {
+    private static DateTime previous(final int weekDay, DateTime startDate) {
         return loopUntil(startDate.minusDays(1),
                 dt -> dt.getDayOfWeek() == weekDay, dt -> dt.minusDays(1));
-//        return loopUntil(startDate.minusDays(1), weekDay, -1);
     }
 
-    // yes, it's more complicated, but I wanted to apply Java 8 FP
-    // (and this way loopUntil is also more general)
-    private DateTime loopUntil(DateTime dateTime,
+    private static DateTime loopUntil(DateTime dateTime,
                 Predicate<DateTime> found, Function<DateTime, DateTime> next) {
-        if (found.test(dateTime)) return dateTime;
-        else return loopUntil(next.apply(dateTime), found, next);
+        return dateTimeStream(dateTime, next)
+                 .filter(found)
+                 .findFirst()
+                 .get();
     }
-//    private DateTime loopUntil(DateTime dateTime, int weekDay, int incrementDay) {
-//        if (dateTime.getDayOfWeek() == weekDay) return dateTime;
-//        else return loopUntil(dateTime.plusDays(incrementDay), weekDay, incrementDay);
-//    }
+
+    private static Stream<DateTime> dateTimeStream(final DateTime startDateTime,
+                final Function<DateTime, DateTime> next) {
+        Iterator<DateTime> dateTimeIter =
+            new Iterator<DateTime>() {
+                private DateTime currentDateTime = startDateTime;
+
+                @Override public boolean hasNext() { return true; }
+
+                @Override public DateTime next() {
+                    DateTime current = currentDateTime;
+                    currentDateTime = next.apply(current);
+                    return current;
+                }
+            };
+
+            return fromIterator(dateTimeIter)
+                     .limit(31); // emergency break
+    }
+
+    private static <T> Stream<T> fromIterator(Iterator<T> iterator) {
+        Iterable<T> iterable = () -> iterator;
+        return StreamSupport.stream(iterable.spliterator(), false);
+    }
 }
