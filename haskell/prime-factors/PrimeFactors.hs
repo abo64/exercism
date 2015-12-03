196190bfeadd4f63d060d7ee3ac407d3de20e592
Achim <abo64@gmx.de>
2015-12-03 16:26:00 +0100

    haskell/prime-factors: solution w/ unfoldr

--- haskell/prime-factors/PrimeFactors.hs
+++ haskell/prime-factors/PrimeFactors.hs
@@ -1,18 +1,18 @@
 module PrimeFactors (primeFactors) where
 
-import Data.List (sort)
+import Data.List (unfoldr)
 
 type PrimeFactors = [Int]
 
 primeFactors :: Int -> PrimeFactors
 primeFactors n =
-  loop n 2 []
+  unfoldr findNext (n, 2)
   where
-    loop :: Int -> Int -> PrimeFactors -> PrimeFactors
-    loop dividend divisor acc
-      | dividend == 1 = sort acc
-      | divisor `goesCleanlyInto` dividend = loop (dividend `div` divisor) divisor (divisor:acc)
-      | otherwise = loop dividend (divisor + 1) acc
+    findNext :: (Int, Int) -> Maybe (Int, (Int,Int))
+    findNext (dividend, divisor)
+      | dividend == 1 = Nothing
+      | divisor `goesCleanlyInto` dividend = Just (divisor, (dividend `quot` divisor, divisor))
+      | otherwise = findNext (dividend, divisor + 1)
 
     goesCleanlyInto :: Int -> Int -> Bool
     goesCleanlyInto divisor dividend =
