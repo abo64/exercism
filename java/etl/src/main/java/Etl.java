bcd755692171c47dee068ffb4cf88200a911236a
Achim <abo64@gmx.de>
2015-12-18 07:11:07 +0100

    java/etl: solution w/ foldLeft

--- java/etl/src/main/java/Etl.java
+++ java/etl/src/main/java/Etl.java
@@ -1,45 +1,30 @@
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
-import java.util.function.Function;
-import java.util.stream.Collector;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
+import java.util.function.BiFunction;
 
 public class Etl {
 
     public Map<String, Integer> transform(Map<Integer, List<String>> oldScores) {
-        Stream<Pair<String, Integer>> newScores =
-            oldScores.entrySet().stream().flatMap(oldScoreToNewScores);
-        return newScores.collect(pairsToMap());
+        return foldLeft(oldScores.entrySet(), new HashMap<>(), oldScoreToNewScores);
     }
 
-    private static final Function<
-      Entry<Integer, List<String>>,
-      Stream<Pair<String, Integer>>> oldScoreToNewScores =
-        oldScore -> {
+    private static final BiFunction<Map<String, Integer>, Entry<Integer, List<String>>,
+            Map<String, Integer>> oldScoreToNewScores =
+        (newScores, oldScore) -> {
             Integer point = oldScore.getKey();
             List<String> letters = oldScore.getValue();
-            Stream<Pair<String, Integer>> newScores =
-                letters.stream().map(l -> new Pair<String, Integer>(l.toLowerCase(), point));
+            // sorry for the side effect - Java 8 FP requires compromises! ;-)
+            letters.stream().forEach(l -> newScores.put(l.toLowerCase(), point));
             return newScores;
         };
 
-    private static final <K,V> Collector<Pair<K,V>, ?, Map<K,V>> pairsToMap() {
-        return Collectors.toMap(Pair::getFirst, Pair::getSecond);
-    }
-
-    // will there ever be built-in Tuples in Java?
-    private static class Pair<F,S> {
-        private F first;
-        private S second;
-
-        private Pair(F first, S second) {
-            this.first = first;
-            this.second = second;
+    private static <A, B> B foldLeft(Iterable<A> as, B z, BiFunction<B, A, B> f) {
+        B result = z;
+        for (A a : as) {
+            result = f.apply(result, a);
         }
-
-        public F getFirst() { return first; }
-        public S getSecond() { return second; }
+        return result;
     }
 }
