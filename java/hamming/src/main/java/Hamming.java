9845e9074a9575ca3aa02c085fcc1731d0e05b42
Achim <abo64@gmx.de>
2015-12-21 09:50:22 +0100

    java/hamming: solution w/ Stream of String indices

--- java/hamming/src/main/java/Hamming.java
+++ java/hamming/src/main/java/Hamming.java
@@ -1,6 +1,5 @@
-import java.util.Iterator;
-import java.util.function.Predicate;
-import java.util.stream.Stream;
+import java.util.function.IntPredicate;
+import java.util.stream.IntStream;
 
 public class Hamming {
 
@@ -8,52 +7,15 @@ public class Hamming {
         if (strand1.length() != strand2.length())
             throw new IllegalArgumentException("different strand lengths");
 
-        Stream<NucleotidePair> nucleotidePairs = pairStrands(strand1, strand2);
-        long count =
-            nucleotidePairs
-                .filter(NucleotidePair::differentNucleotides)
-                .count();
-        return (int)count;
+        IntStream nucleotidePositions = IntStream.range(0, strand1.length());
+
+        return (int)
+            nucleotidePositions
+              .filter(differentNucleotides(strand1, strand2))
+              .count();
     }
 
-    private static Stream<NucleotidePair> pairStrands(String strand1, String strand2) {
-        return zip(characterStream(strand1), characterStream(strand2))
-                 .map(NucleotidePair::new);
-    }
-
-    private static <A,B> Stream<Pair<A,B>> zip(Stream<A> as, Stream<B> bs) {
-        // is there a better way than using an iterator?
-        Iterator<B> bIter = bs.iterator();
-        return as.map(a -> bIter.hasNext() ? new Pair<A,B>(a, bIter.next()) : null)
-                 .filter(notNull);
-    }
-
-    private static final Predicate<Object> notNull = o -> o != null;
-
-    private static Stream<Character> characterStream(String str) {
-        return str.chars().mapToObj(i -> (char)i);
-    }
-
-    private static class NucleotidePair extends Pair<Character, Character> {
-        private NucleotidePair(Pair<Character, Character> pair) {
-            super(pair.first, pair.second);
-        }
-
-        boolean differentNucleotides() {
-            return getFirst() != getSecond();
-        }
-    }
-
-    private static class Pair<F,S> {
-        private F first;
-        private S second;
-
-        private Pair(F first, S second) {
-            this.first = first;
-            this.second = second;
-        }
-
-        public F getFirst() { return first; }
-        public S getSecond() { return second; }
+    private static IntPredicate differentNucleotides(String strand1, String strand2) {
+        return position -> strand1.charAt(position) != strand2.charAt(position);
     }
 }
