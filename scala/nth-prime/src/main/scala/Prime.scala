447530fc0267abea5fd0630ede1c15abf2dfae78
Achim <abo64@gmx.de>
2015-12-22 07:00:50 +0100

    scala/nth-prime: simple recursion

--- scala/nth-prime/src/main/scala/Prime.scala
+++ scala/nth-prime/src/main/scala/Prime.scala
@@ -1,28 +1,26 @@
 import collection.mutable
 import scala.annotation.tailrec
+import scala.collection.mutable.ListBuffer
 
 object Prime {
-  def nth(n: Int): Int = {
-    lazy val lastN = cache.keys max
-    def lastPrime = cache(lastN)
+  type Prime = Int
 
-    cache getOrElse(n, nextPrime(n, lastN, lastPrime))
-  }
+  def nth(n: Int): Prime =
+    loop(n, 3, ListBuffer(2))
 
-  private val cache: mutable.Map[Int,Int] = mutable.Map(1 -> 2)
-  private def storePrime(n: Int, prime: Int) = cache += (n -> prime)
-
-  // Sieve of Eratosthenes, but w/o nested Streams to avoid OutOfMemoryError
+  // Sieve of Eratosthenes
   @tailrec
-  private def nextPrime(n: Int, i: Int, lastPrime: Int): Int = {
-    if (n == i) lastPrime
+  private def loop(n: Int, candidate: Int, primes: ListBuffer[Int]): Prime = {
+    if (primes.size == n) primes.last
     else {
-      val previousPrimes = cache.values
-      def isPrime(p: Int) = previousPrimes forall (p % _ != 0)
-      val nextPrimes = Stream.from(lastPrime + 1) filter isPrime
-      val prime = nextPrimes.head
-      storePrime(i + 1, prime)
-      nextPrime(n, i + 1, prime)
+      def notDivisibleBy(i: Int) = candidate % i != 0
+      def squareLE(i: Int) = i*i <= candidate
+
+      val relevantPrimes = primes takeWhile squareLE
+      val candidateIsPrime = relevantPrimes forall notDivisibleBy
+      if (candidateIsPrime) primes += candidate
+      val nextCandidate = candidate + 1
+      loop(n, nextCandidate, primes)
     }
   }
 }
