75bc0c2ccc8205282acc897e7d3b0ee0bc869e3b
Achim <abo64@gmx.de>
2016-01-07 13:07:15 +0100

    scala/forth: implemented word definitions

--- scala/forth/src/main/scala/Forth.scala
+++ scala/forth/src/main/scala/Forth.scala
@@ -1,155 +1,154 @@
 import ForthError.ForthError
-import scala.collection.mutable
 import scala.util.Try
+import Forth._
 
-class Forth extends ForthEvaluator {
 
-  private val state = new ForthState()
+case class ForthState(stack: List[Int] = List(),
+    newWords:Map[WordName, WordDefinition] = Map()) extends ForthEvaluatorState
+{
+  def push(elem: Int): ForthOpResult[Unit] =
+    Right(((), copy(stack = elem::stack)))
 
-  import Forth._
-
-  override def eval(text: String): Either[ForthError, ForthEvaluatorState] = {
-    val terms = parseText(text)
-    val emptyEvaluationState: EvaluationState = Right(new ForthState())
-    terms.foldLeft(emptyEvaluationState) {
-      (evaluationState, term) => term.evaluate(evaluationState)
-    }
-  }
-}
-object Forth {
-  def parseText(text: String): Seq[Term] = {
-    if (text.trim.isEmpty) Seq()
-    else {
-      val termStrs = text.split("""[\W&&[^+-/*]]""")
-      termStrs map toTerm
-    }
-  }
-
-  private[this] def toTerm(termStr: String): Term = termStr.toUpperCase match {
-    case Number(number) => number
-    case Plus(plus) => plus
-    case Minus(minus) => minus
-    case Mult(mult) => mult
-    case Div(div) => div
-    case Dup(dup) => dup
-    case Drop(drop) => drop
-    case Swap(swap) => swap
-    case Over(over) => over
-  }
-
-  type EvaluationState = Either[ForthError, ForthState]
-  type IntResult = Either[ForthError, Int]
-
-  sealed trait Term {
-    def evaluate(state: EvaluationState): EvaluationState
-  }
-  case class Number(number: Int) extends Term {
-    override def evaluate(state: EvaluationState): EvaluationState =
-      state.right.flatMap (_.push(number))
-  }
-  object Number {
-    def unapply(str: String): Option[Number] =
-      Try(Number(str.toInt)).toOption
-  }
-
-  trait Operator extends Term {
-    val op: String
-    def unapply(str: String): Option[this.type] =
-      Option(str) filter (_ == op) map (_ => this)
-  }
-
-  trait BinaryOp extends Operator {
-    val operation: (Int, Int) => IntResult
-    override def evaluate(evaluationState: EvaluationState): EvaluationState =
-      for {
-        state <- evaluationState.right
-        arg2 <- state.pop.right
-        arg1 <- state.pop.right
-        result <- operation(arg1, arg2).right
-        newState <- state.push(result).right
-      } yield newState
-  }
-
-  case object Plus extends BinaryOp {
-    override val op = "+"
-    override val operation = (arg1: Int, arg2:Int) => Right(arg1 + arg2)
-  }
-
-  case object Minus extends BinaryOp {
-    override val op = "-"
-    override val operation = (arg1: Int, arg2:Int) => Right(arg1 - arg2)
-  }
-
-  case object Mult extends BinaryOp {
-    override val op = "*"
-    override val operation = (arg1: Int, arg2:Int) => Right(arg1 * arg2)
-  }
-
-  case object Div extends BinaryOp {
-    override val op = "/"
-    override val operation = (dividend:Int, divisor:Int) =>
-      if (divisor == 0) Left(ForthError.DivisionByZero)
-      else Right(dividend / divisor)
-  }
-
-  case object Dup extends Operator {
-    override val op = "DUP"
-    override def evaluate(evaluationState: EvaluationState): EvaluationState =
-      for {
-        state <- evaluationState.right
-        next <- state.pop.right
-        state1 <- state.push(next).right
-        state2 <- state.push(next).right
-      } yield state2
-  }
-
-  case object Drop extends Operator {
-    override val op = "DROP"
-    override def evaluate(evaluationState: EvaluationState): EvaluationState =
-      for {
-        state <- evaluationState.right
-        _ <- state.pop.right
-      } yield state
-  }
-
-  case object Swap extends Operator {
-    override val op = "SWAP"
-    override def evaluate(evaluationState: EvaluationState): EvaluationState =
-      for {
-        state <- evaluationState.right
-        first <- state.pop.right
-        second <- state.pop.right
-        state1 <- state.push(first).right
-        state2 <- state.push(second).right
-      } yield state2
-  }
-
-  case object Over extends Operator {
-    override val op = "OVER"
-    override def evaluate(evaluationState: EvaluationState): EvaluationState =
-      for {
-        state <- evaluationState.right
-        first <- state.pop.right
-        second <- state.pop.right
-        state1 <- state.push(second).right
-        state2 <- state.push(first).right
-        state3 <- state.push(second).right
-      } yield state3
-  }
-}
-
-class ForthState extends ForthEvaluatorState {
-
-  private val stack = mutable.Stack[Int]()
-
-  def push(elem: Int): Right[ForthError, ForthState] = {
-    stack.push(elem)
-    Right(this)
-  }
-
-  def pop: Either[ForthError, Int] =
+  // FP style: return value and new state
+  def pop: ForthOpResult[Int] =
     if (stack.isEmpty) Left(ForthError.StackUnderflow)
-    else Right(stack.pop)
+    else Right((stack.head, copy(stack = stack.tail)))
+
+  def addNewWord(wordName: WordName, wordDefinition: WordDefinition): ForthOpResult[Unit] = {
+    val newWord = (wordName, wordDefinition)
+    Right(((), copy(newWords = newWords + newWord)))
+  }
 
   override def toString = stack.reverse mkString " "
 }
+
+class Forth extends ForthEvaluator {
+  override def eval(input: String): Either[ForthError, ForthEvaluatorState] =
+    evalForth(parseInput(input), EmptyEvaluationState)
+}
+
+object Forth {
+  type EvaluationState = Either[ForthError, ForthState]
+  implicit class EvaluationStateOps(self: EvaluationState) {
+    def >>=[A](f: ForthResult): EvaluationState = self.right.flatMap(f)
+  }
+  val EmptyEvaluationState: EvaluationState = Right(ForthState())
+
+  // FP style: return value and new state
+  type ForthOpResult[+A] = Either[ForthError, (A, ForthState)]
+  type ForthOp[+A] = ForthState => ForthOpResult[A]
+  type ForthResult = ForthState => EvaluationState
+  implicit def forthOpToForthResult(forthOp: ForthOp[_]): ForthResult =
+    fs =>  forthOp(fs).right.map { case (_, state) => state }
+
+  type ParsedInput = List[String]
+  type WordName = String
+  type WordDefinition = List[String]
+
+  def evalForth(parsedInput: ParsedInput, state: EvaluationState): EvaluationState =
+    parsedInput match {
+      case Nil => state
+      case ":"::wordName::xs => {
+        if (parseNumber(wordName).isDefined) Left(ForthError.InvalidWord)
+        else {
+          val (newWordDef, _::parsedInputRest) = xs span (_ != ";")
+          val nextState = state >>= doAddNewWord(wordName.toUpperCase, newWordDef)
+          evalForth(parsedInputRest, nextState)
+        }
+      }
+      case x::xs => {
+        val maybeWordDef: Option[WordDefinition] =
+          state.fold(_ => None, _.newWords.get(x.toUpperCase))
+        val nextParsedInput = maybeWordDef.map (_ ++ xs) getOrElse xs
+        val maybeNumber = parseNumber(x)
+        val nextState: EvaluationState =
+          if (maybeNumber.isDefined) state >>= doPush(maybeNumber.get)
+          else if (maybeWordDef.isDefined) state
+          else x.toUpperCase match {
+            case "+" => state >>= doBinaryOp(_ + _)
+            case "-" => state >>= doBinaryOp(_ - _)
+            case "*" => state >>= doBinaryOp(_ * _)
+            case "/" => state >>= doDiv
+            case "DUP" => state >>= doDup
+            case "DROP" => state >>= doDrop
+            case "SWAP" => state >>= doSwap
+            case "OVER" => state >>= doOver
+            case _ => Left(ForthError.InvalidWord)
+          }
+
+        evalForth(nextParsedInput, nextState)
+      }
+    }
+
+  def parseInput(input: String): ParsedInput = {
+    if (input.trim.isEmpty) List()
+    else input.split("""[\W&&[^+-/*:;]]""").filter(_.nonEmpty) toList
+  }
+
+  def parseNumber(str: String): Option[Int] =
+    Try(str.toInt).toOption
+
+  def map[A,B](s: ForthOp[A])(f: A => B): ForthOp[B] =
+    fs =>
+      s(fs).right.map {
+        case (a, fs1) => (f(a), fs1)
+      }
+
+  // for-comprehensions w/ Either have a bug when matching tuples:
+  // https://issues.scala-lang.org/browse/SI-5589
+  // so I had to go for flatMap instead
+  def flatMap[A,B](f: ForthOp[A])(g: A => ForthOp[B]): ForthOp[B] =
+    fs =>
+      f(fs).right.flatMap {
+        case (a, fs1) => g(a)(fs1)
+      }
+
+  def doPush(elem: Int): ForthOp[Unit] = _.push(elem)
+  def doPop: ForthOp[Int] = _.pop
+  def doAddNewWord(wn: WordName, wd: WordDefinition): ForthOp[Unit] = _.addNewWord(wn, wd)
+
+  def doBinaryOp(op: (Int,Int) => Int): ForthResult =
+    flatMap(doPop) { x =>
+      flatMap(doPop) { y =>
+        doPush(op(y, x))
+      }
+    }
+
+  def doDiv: ForthResult =
+    flatMap(doPop) { y =>
+      flatMap(doPop) { z =>
+        if (y != 0) doPush(z / y)
+        else _ => Left(ForthError.DivisionByZero)
+      }
+    }
+
+  def doDup: ForthResult =
+    flatMap(doPop) { y =>
+      flatMap(doPush(y)) { _ =>
+        doPush(y)
+      }
+    }
+
+  def doDrop: ForthResult = doPop
+
+  def doSwap: ForthResult =
+    flatMap(doPop) { x =>
+      flatMap(doPop) { y =>
+        flatMap(doPush(x)) { _ =>
+          doPush(y)
+        }
+      }
+    }
+
+  def doOver: ForthResult =
+    flatMap(doPop) { x =>
+      flatMap(doPop) { y =>
+        flatMap(doPush(y)) { _ =>
+          flatMap(doPush(x)) { _ =>
+            doPush(y)
+          }
+        }
+      }
+    }
+}
+
