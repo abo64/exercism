6683cfc34067ba95cec44f648010ba06aaeb4d4e
Achim <abo64@gmx.de>
2016-01-08 10:31:44 +0100

    java/word-count: simplification using built-in Collector

--- java/word-count/src/main/java/WordCount.java
+++ java/word-count/src/main/java/WordCount.java
@@ -1,46 +1,16 @@
-import java.util.List;
+import static java.util.function.Function.identity;
+import static java.util.stream.Collectors.groupingBy;
+import static java.util.stream.Collectors.summingInt;
+
+import java.util.Arrays;
 import java.util.Map;
-import java.util.function.Function;
-import java.util.stream.Collector;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class WordCount {
 
-    public Map<String, Integer> Phrase(String phrase) {
-        Map<String, List<String>> wordsByWord =
-            wordStream(phrase)
-                .map(word -> word.toLowerCase())
-                .collect(Collectors.groupingBy(Function.identity()));
-        return mapValues(wordsByWord, words -> words.size());
-    }
-
-	private Stream<String> wordStream(String phrase) {
-        String[] words = phrase.split("\\W+");
-        return Stream.of(words);
-    }
-
-    private static final <K,V, U> Map<K,U> mapValues(Map<K,V> source, Function<V, U> f) {
-        return
-          source.entrySet().stream()
-            .map(e -> new Pair<K,U>(e.getKey(), f.apply(e.getValue())))
-            .collect(pairsToMap());
-    }
-
-    private static final <K,V> Collector<Pair<K,V>, ?, Map<K,V>> pairsToMap() {
-        return Collectors.toMap(Pair::getFirst, Pair::getSecond);
-    }
-
-    private static class Pair<F,S> {
-        private F first;
-        private S second;
-
-        private Pair(F first, S second) {
-            this.first = first;
-            this.second = second;
-        }
-
-        public F getFirst() { return first; }
-        public S getSecond() { return second; }
+    public Map<String, Integer> phrase(String phrase) {
+        String[] tokens = phrase.split("\\W+");
+        return Arrays.stream(tokens)
+                .map(String::toLowerCase)
+                .collect(groupingBy(identity(), summingInt(e -> 1)));
     }
 }
