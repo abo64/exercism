980f59d65100d2f6aec0ae533bec7cfa5d9bf5c5
Achim <abo64@gmx.de>
2016-01-08 11:19:16 +0100

    haskell/leap: cosmetics

--- haskell/leap/LeapYear.hs
+++ haskell/leap/LeapYear.hs
@@ -2,13 +2,8 @@ module LeapYear (isLeapYear) where
 
 isLeapYear :: Int -> Bool
 isLeapYear year =
-  let yearDivisibleBy = evenlyDivisibleBy year
-  in yearDivisibleBy 4 &&
-     (not (yearDivisibleBy 100) || yearDivisibleBy 400)
+  yearDivisibleBy 4 &&
+     (not . yearDivisibleBy) 100 || yearDivisibleBy 400
   where
+    yearDivisibleBy = evenlyDivisibleBy year
     evenlyDivisibleBy x n = (x `mod` n) == 0
-
---evenlyDivisibleBy :: Int -> Int -> Bool
---evenlyDivisibleBy x n = (x `mod` n) == 0
-
-
