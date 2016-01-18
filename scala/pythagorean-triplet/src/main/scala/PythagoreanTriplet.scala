7d82a63db018ea742fc53a62f8767e93134fb6ca
Achim <abo64@gmx.de>
2016-01-18 06:50:28 +0100

    scala/pythagorean-triplet: optimization and point-free style

--- scala/pythagorean-triplet/src/main/scala/PythagoreanTriplet.scala
+++ scala/pythagorean-triplet/src/main/scala/PythagoreanTriplet.scala
@@ -1,28 +1,27 @@
 object PythagoreanTriplet {
-  type Triplet = (Int,Int,Int)
+  type Triplet = (Int, Int, Int)
   type PythagoreanTriplets = Seq[Triplet]
 
-  def isPythagorean(triplet: Triplet): Boolean = {
-    def sq(x: Int) = x*x
+  private val isPythagoreanSeq: Seq[Int] => Boolean = {
+    def sq(x: Int) = x * x
 
-    val (a,b, c) = triplet
-    sq(a) + sq(b) == sq(c) || sq(c) + sq(b) == sq(a) || sq(a) + sq(c) == sq(b)
+    { case Seq(a, b, c) => sq(a) + sq(b) == sq(c) }
+  }
+
+  val isPythagorean: Triplet => Boolean = {
+    val toSeq: Triplet => Seq[Int] = { case (a, b, c) => Seq(a, b, c) }
+
+    toSeq andThen (_.permutations) andThen (_.exists(isPythagoreanSeq))
   }
 
   def pythagoreanTriplets(start: Int, end: Int): PythagoreanTriplets = {
-    def sortedTriplet(a: Int, b: Int, c: Int): Triplet = {
-      val Seq(x, y, z) = Seq(a,b,c).sorted
-      (x,y,z)
-    }
-
-    val range = start to end
     val triplets =
       for {
-        a <- range
-        b <- range
-        c <- range if isPythagorean((a, b, c))
-        triplet = sortedTriplet(a, b, c)
-      } yield triplet
-    triplets.distinct
+        c <- start to end
+        b <- start to c
+        a <- start to b if isPythagoreanSeq(Seq(a, b, c))
+      } yield (a,b,c)
+
+    triplets.sorted
   }
 }
