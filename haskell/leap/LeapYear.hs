78dfdd35b97d635a36802e88707d56a1b3e74c1e
Achim <abo64@gmx.de>
2016-01-26 07:58:00 +0100

    haskell/leap: point-free style

--- haskell/leap/LeapYear.hs
+++ haskell/leap/LeapYear.hs
@@ -5,5 +5,4 @@ isLeapYear year =
   yearDivisibleBy 4 &&
      (not . yearDivisibleBy) 100 || yearDivisibleBy 400
   where
-    yearDivisibleBy = evenlyDivisibleBy year
-    evenlyDivisibleBy x n = (x `mod` n) == 0
+    yearDivisibleBy = (== 0) . mod year
