0e6e0c564b0505875dddb83e29bacf85878d77b9
Achim <abo64@gmx.de>
2016-02-09 10:56:08 +0100

    haskell/grade-school: sorted and grade point-free

--- haskell/grade-school/School.hs
+++ haskell/grade-school/School.hs
@@ -3,25 +3,19 @@ module School (School, add, empty, sorted, grade) where
 import Data.Map (Map)
 import qualified Data.Map as M
 import Data.List (sort)
-import Data.Maybe (fromMaybe)
 
 type Grade = Int
 type Name = String
 type School = Map Grade [Name]
 
 add :: Grade -> Name -> School -> School
-add grad name =
-  M.insertWith (++) grad [name]
+add grad name = M.insertWith (++) grad [name]
 
 empty :: School
 empty = M.empty
 
 sorted :: School -> [(Grade, [Name])]
-sorted school =
-  M.toList withSortedValues
-  where
-    withSortedValues = M.map sort school
+sorted = M.toList . M.map sort
 
 grade :: Grade -> School -> [Name]
-grade grad school =
-  fromMaybe [] (M.lookup grad school)
+grade = M.findWithDefault []
