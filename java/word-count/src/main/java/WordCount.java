6e444723465aebab4cca5a956fcdbe67846be9db
Achim <abo64@gmx.de>
2016-03-03 08:35:20 +0100

    WordCount: use Pattern.splitAsStream

--- java/word-count/src/main/java/WordCount.java
+++ java/word-count/src/main/java/WordCount.java
@@ -2,15 +2,15 @@ import static java.util.function.Function.identity;
 import static java.util.stream.Collectors.groupingBy;
 import static java.util.stream.Collectors.summingInt;
 
-import java.util.Arrays;
 import java.util.Map;
+import java.util.regex.Pattern;
 
 public class WordCount {
 
+    private static final Pattern SplitWords = Pattern.compile("\\W+");
+
     public Map<String, Integer> phrase(String phrase) {
-        String[] tokens = phrase.split("\\W+");
-        return Arrays.stream(tokens)
-                .map(String::toLowerCase)
+        return SplitWords.splitAsStream(phrase.toLowerCase())
                 .collect(groupingBy(identity(), summingInt(e -> 1)));
     }
 }
