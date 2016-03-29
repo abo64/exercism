d68299055ad42600de08304a38abd9917e0fb3e1
Achim <abo64@gmx.de>
2016-03-29 14:24:44 +0200

    java/accumulate: solution w/ reduce

--- java/accumulate/src/main/java/Accumulate.java
+++ java/accumulate/src/main/java/Accumulate.java
@@ -1,24 +1,37 @@
 import java.util.ArrayList;
 import java.util.List;
 import java.util.function.BiFunction;
+import java.util.function.BinaryOperator;
 import java.util.function.Function;
 
 public class Accumulate {
 
     public static <T> List<T> accumulate(List<T> ts, Function<T, T> f) {
-        return foldLeft(ts,
-                 new ArrayList<T>(),
-                 (fts, t) -> {
-                     fts.add(f.apply(t));
-                     return fts;
-                 });
+        BiFunction<List<T>,T,List<T>> accumulator =
+            (fts, t) -> {
+                fts.add(f.apply(t));
+                return fts;
+            };
+
+        BinaryOperator<List<T>> takeSecond =
+            (fts1, fts2) -> fts2;
+
+        return ts.stream()
+                 .reduce(new ArrayList<T>(), accumulator, takeSecond);
+
+//        return foldLeft(ts,
+//                 new ArrayList<T>(),
+//                 (fts, t) -> {
+//                     fts.add(f.apply(t));
+//                     return fts;
+//                 });
     }
 
-    private static <A, B> B foldLeft(Iterable<A> as, B z, BiFunction<B,A,B> f) {
-        B result = z;
-        for (A a : as) {
-            result = f.apply(result, a);
-        }
-        return result;
-    }
+//    private static <A, B> B foldLeft(Iterable<A> as, B z, BiFunction<B,A,B> f) {
+//        B result = z;
+//        for (A a : as) {
+//            result = f.apply(result, a);
+//        }
+//        return result;
+//    }
 }
