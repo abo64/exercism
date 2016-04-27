c8fa1e64bd2e4de29632cce22d5206b7c470c6c5
Achim <abo64@gmx.de>
2016-04-27 13:34:46 +0200

    java/trinary: use reduce

--- java/trinary/src/main/java/Trinary.java
+++ java/trinary/src/main/java/Trinary.java
@@ -1,4 +1,3 @@
-import java.util.function.BiFunction;
 import java.util.function.Predicate;
 import java.util.stream.Stream;
 
@@ -11,21 +10,13 @@ public class Trinary {
     }
 
     private static int trinaryToInt(String trinary) {
-        Iterable<Character> trinaryDigits = characterStream(trinary)::iterator;
-        return foldLeft(trinaryDigits, 0,
-                 (acc, trinaryDigit) -> acc * 3 + Character.getNumericValue(trinaryDigit));
+        return characterStream(trinary)
+          .map(Character::getNumericValue)
+          .reduce(0, (acc, trinaryDigit) -> acc * 3 + trinaryDigit);
     }
 
     private static final Predicate<Character> isTrinaryDigit =
         c -> "012".indexOf(c) != -1;
-
-    private static <A, B> B foldLeft(Iterable<A> as, B z, BiFunction<B, A, B> f) {
-        B result = z;
-        for (A a : as) {
-            result = f.apply(result, a);
-        }
-        return result;
-    }
 
     private static Stream<Character> characterStream(CharSequence str) {
         return str.chars().mapToObj(i -> (char)i);
