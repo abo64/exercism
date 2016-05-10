dac526832bd9cfcd2963c3f6099a153ecc38e998
Achim <abo64@gmx.de>
2016-05-10 08:23:50 +0200

    java/rna-transcription: use Map.get directly

--- java/rna-transcription/src/main/java/RnaTranscription.java
+++ java/rna-transcription/src/main/java/RnaTranscription.java
@@ -1,7 +1,5 @@
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Optional;
-import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -9,24 +7,17 @@ public class RnaTranscription {
 
     public static Object ofDna(String dna) {
         Stream<Character> rnaNucleotides =
-           characterStream(dna).map(toRnaNucleotide);
+           characterStream(dna).map(RnaMap::get);
 
         return mkString(rnaNucleotides, "");
     }
 
-    private static final Function<Character, Character> toRnaNucleotide;
+    private static final Map<Character, Character> RnaMap = new HashMap<>();
     static {
-        Map<Character, Character> toRnaMap = new HashMap<>();
-        toRnaMap.put('G', 'C');
-        toRnaMap.put('C', 'G');
-        toRnaMap.put('T', 'A');
-        toRnaMap.put('A', 'U');
-        toRnaNucleotide = mapToFunction(toRnaMap, Optional.empty());
-    }
-
-    private static <K,V> Function<K,V> mapToFunction(Map<K,V> map, Optional<V> defaultValue) {
-        return key -> defaultValue.isPresent() ?
-            map.getOrDefault(key, defaultValue.get()) : map.get(key);
+        RnaMap.put('G', 'C');
+        RnaMap.put('C', 'G');
+        RnaMap.put('T', 'A');
+        RnaMap.put('A', 'U');
     }
 
     private static <A> String mkString(Stream<A> as, CharSequence delimiter) {
