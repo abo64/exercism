ff3c8a7e1240a979e77ae7d4c6bde0bccbccbb0e
Achim <abo64@gmx.de>
2016-05-13 07:15:41 +0200

    haskell/accumulate: point-free solution w/ foldr

--- haskell/accumulate/Accumulate.hs
+++ haskell/accumulate/Accumulate.hs
@@ -1,6 +1,5 @@
 module Accumulate (accumulate) where
 
 accumulate :: (a -> b) -> [a] -> [b]
-accumulate _ [] = []
-accumulate f (a:as) = (f a) : accumulate f as
+accumulate f = foldr ((:) . f) []
 
