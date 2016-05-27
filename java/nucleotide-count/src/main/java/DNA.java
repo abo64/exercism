125004f33b52334077c84425591bb49d3f4b2340
Achim <abo64@gmx.de>
2016-05-27 08:36:16 +0200

    java/nucleotide-count: solution w/ groupingBy only

--- java/nucleotide-count/src/main/java/DNA.java
+++ java/nucleotide-count/src/main/java/DNA.java
@@ -1,8 +1,6 @@
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.List;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.Map;
-import java.util.Set;
 import java.util.function.Function;
 import java.util.stream.Collector;
 import java.util.stream.Collectors;
@@ -16,53 +14,37 @@ public class DNA {
     }
 
     public Integer count(Character nucleotide) {
-        if (!Nucleotides.contains(nucleotide))
+        if (!Nucleotides.containsKey(nucleotide))
             throw new IllegalArgumentException("not a nucleotide: " + nucleotide);
         long count = nucleotideStream().filter(c -> c == nucleotide).count();
         return new Long(count).intValue();
     }
 
     public Map<Character,Integer> nucleotideCounts() {
-        Map<Character, List<Character>> charsByChar =
-            nucleotideStream().collect(Collectors.groupingBy(Function.identity()));
-        Map<Character, Integer> nucleotideCounts = mapValues(charsByChar, l -> l.size());
-        putZerosIfAbsent(nucleotideCounts);
-        return nucleotideCounts;
+        Map<Character, Integer> nucleotideCounts = nucleotideStream()
+                 .collect(Collectors.groupingBy(Function.identity(), intCounting));
+        return withZeroCounts(nucleotideCounts);
+    }
+
+    private Collector<Character, ?, Integer> intCounting =
+        Collectors.reducing(0, e -> 1, (a,b) -> a + b);
+
+    private Map<Character, Integer> withZeroCounts(Map<Character, Integer> nucleotideCounts) {
+        Map<Character,Integer> result = new HashMap<>(Nucleotides);
+        result.putAll(nucleotideCounts);
+        return result;
     }
 
     private Stream<Character> nucleotideStream() {
         return dna.chars().mapToObj(i -> (char)i);
     }
 
-    private static final Set<Character> Nucleotides =
-      new HashSet<>(Arrays.asList('A', 'C', 'G', 'T'));
-
-    private static final void putZerosIfAbsent(Map<Character,Integer> nucleotideCounts) {
-        Nucleotides.forEach(nucleotide -> nucleotideCounts.putIfAbsent(nucleotide, 0));
-    }
-
-    private static final <K,V, U> Map<K,U> mapValues(Map<K,V> source, Function<V, U> f) {
-        return
-          source.entrySet().stream()
-            .map(e -> new Pair<K,U>(e.getKey(), f.apply(e.getValue())))
-            .collect(pairsToMap());
-    }
-
-    private static final <K,V> Collector<Pair<K,V>, ?, Map<K,V>> pairsToMap() {
-        return Collectors.toMap(Pair::getFirst, Pair::getSecond);
-    }
-
-    // will there ever be built-in Tuples in Java?
-    private static class Pair<F,S> {
-        private F first;
-        private S second;
-
-        private Pair(F first, S second) {
-            this.first = first;
-            this.second = second;
-        }
-
-        public F getFirst() { return first; }
-        public S getSecond() { return second; }
-    }
+    @SuppressWarnings("serial")
+    private static final Map<Character,Integer> Nucleotides =
+        Collections.unmodifiableMap(new HashMap<Character,Integer>(){{
+            put('A',0);
+            put('C',0);
+            put('G', 0);
+            put('T', 0);
+        }});
 }
