8a901b6ecf81c1073070bcbd55850c251db8a93e
Achim <abo64@gmx.de>
2016-06-17 07:20:59 +0200

    scala/accumulate: solution w/foldRight in Haskell style

--- scala/accumulate/src/main/scala/Accumulate.scala
+++ scala/accumulate/src/main/scala/Accumulate.scala
@@ -1,6 +1,13 @@
 import scala.annotation.tailrec
 
 class Accumulate {
-  def accumulate[A,B](f: A => B, as: List[A]): List[B] =
-    as.foldLeft(List.empty[B]){(bs,a) => bs :+ f(a)}
+  def accumulate[A,B](f: A => B, as: List[A]): List[B] = {
+//    as.foldRight(List.empty[B]){(a, bs) => f(a) :: bs}
+
+    // just for fun in "Haskell style"
+    // Haskell is (not really surprisingly) more elegant here:
+    // accumulate f = foldr ((:) . f) []
+    val cons: B => (List[B] => List[B]) = b => (b :: _)
+    as.foldRight(List.empty[B])(Function.uncurried(cons compose f))
+  }
 }
