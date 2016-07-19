ecce4f427eb240209879266b79fed9d854f94596
Achim <abo64@gmx.de>
2016-07-19 07:29:40 +0200

    java/etl: solution w/ Collectors.reducing

--- java/etl/src/main/java/Etl.java
+++ java/etl/src/main/java/Etl.java
@@ -2,31 +2,35 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
-import java.util.function.BiFunction;
 import java.util.function.BinaryOperator;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public class Etl {
 
     public Map<String, Integer> transform(Map<Integer, List<String>> oldScores) {
         return oldScores.entrySet().stream()
-                 .reduce(new HashMap<String, Integer>(), oldScoreToNewScores, putAll);
+                .collect(Collectors.reducing(new HashMap<String, Integer>(),
+                         oldScoreToNewScores, mergeMaps));
     }
 
-    private static final BinaryOperator<Map<String, Integer>> putAll =
-        (accMap, map) -> {
-            accMap.putAll(map);
-            return accMap;
+    private static final Function<Entry<Integer, List<String>>,
+        Map<String, Integer>> oldScoreToNewScores =
+            (oldScore) -> {
+              Function<String,Integer> point = constant(oldScore.getKey());
+              List<String> letters = oldScore.getValue();
+              return letters.stream()
+                       .map(String::toLowerCase)
+                       .collect(Collectors.toMap(Function.identity(), point));
+            };
+
+    private static final BinaryOperator<Map<String, Integer>> mergeMaps =
+        (map1, map2) -> {
+            map1.putAll(map2);
+            return map1;
         };
 
-    private static final BiFunction<Map<String, Integer>, Entry<Integer, List<String>>,
-            Map<String, Integer>> oldScoreToNewScores =
-        (newScores, oldScore) -> {
-            Integer point = oldScore.getKey();
-            List<String> letters = oldScore.getValue();
-            // sorry for the side effect - Java 8 FP requires compromises! ;-)
-            letters.stream()
-              .map(String::toLowerCase)
-              .forEach(l -> newScores.put(l, point));
-            return newScores;
-        };
+    private static final <T,U> Function<T,U> constant(U u) {
+        return (ignored) -> u;
+    }
 }
