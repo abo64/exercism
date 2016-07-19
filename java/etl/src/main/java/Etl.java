b1a88419e91a9a9c97948b78e6400445e40dcf34
Achim <abo64@gmx.de>
2016-07-19 07:53:20 +0200

    java/etl: solution w/ flatMap

--- java/etl/src/main/java/Etl.java
+++ java/etl/src/main/java/Etl.java
@@ -1,36 +1,28 @@
-import java.util.HashMap;
+import java.util.AbstractMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
-import java.util.function.BinaryOperator;
 import java.util.function.Function;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Etl {
 
     public Map<String, Integer> transform(Map<Integer, List<String>> oldScores) {
         return oldScores.entrySet().stream()
-                .collect(Collectors.reducing(new HashMap<String, Integer>(),
-                         oldScoreToNewScores, mergeMaps));
+                .flatMap(oldScoreToNewScores)
+                .collect(Collectors.toMap(Entry::getKey , Entry::getValue));
     }
 
     private static final Function<Entry<Integer, List<String>>,
-        Map<String, Integer>> oldScoreToNewScores =
-            (oldScore) -> {
-              Function<String,Integer> point = constant(oldScore.getKey());
-              List<String> letters = oldScore.getValue();
-              return letters.stream()
-                       .map(String::toLowerCase)
-                       .collect(Collectors.toMap(Function.identity(), point));
+        Stream<Entry<String, Integer>>> oldScoreToNewScores =
+            oldScore -> {
+                Integer point = oldScore.getKey();
+                Function<String,Entry<String, Integer>> toNewScore =
+                    letter -> new AbstractMap.SimpleEntry<>(letter, point);
+                List<String> letters = oldScore.getValue();
+                return letters.stream()
+                         .map(String::toLowerCase)
+                         .map(toNewScore);
             };
-
-    private static final BinaryOperator<Map<String, Integer>> mergeMaps =
-        (map1, map2) -> {
-            map1.putAll(map2);
-            return map1;
-        };
-
-    private static final <T,U> Function<T,U> constant(U u) {
-        return (ignored) -> u;
-    }
 }
