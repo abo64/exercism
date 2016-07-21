c67c60c04e5769260edee27c3104d29d0229623c
Achim <abo64@gmx.de>
2016-07-21 07:05:50 +0200

    haskell/grains: square returns Maybe, use Bounded typeclass

--- haskell/grains/Grains.hs
+++ haskell/grains/Grains.hs
@@ -1,14 +1,21 @@
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 module Grains (square, total) where
 
-type Grains = Integer
-type ChessboardSquares = Int
+import Data.Maybe (mapMaybe)
 
-square :: ChessboardSquares -> Grains
-square =
-  (2^) . pred
+newtype ChessboardSquare = ChessboardSquare Int
+  deriving (Num, Eq, Enum, Ord, Real, Integral)
 
-total :: Grains
-total =
-  sum $ map square allChessboardSquares
-    where
-      allChessboardSquares = [1 .. 64]
+instance Bounded (ChessboardSquare) where
+  minBound = 1
+  maxBound = 64
+
+square :: Integral a => ChessboardSquare -> Maybe a
+square n
+  | minBound <= n && n <= maxBound = Just $ 2 ^ pred n
+  | otherwise = Nothing
+
+total :: Integer
+total = sum $ mapMaybe square allChessboardSquares
+  where
+    allChessboardSquares = [minBound .. maxBound]
