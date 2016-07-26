bf26c9c930b83cc1687e145cd031d5f3dd26c7e4
Achim <abo64@gmx.de>
2016-07-26 09:21:22 +0200

    haskell/rna-transcription: use Traversable

--- haskell/rna-transcription/DNA.hs
+++ haskell/rna-transcription/DNA.hs
@@ -1,9 +1,10 @@
 module DNA (toRNA) where
 
 import qualified Data.Map as Map
+import Data.Traversable
 
 toRnaNucleotide :: Map.Map Char Char
 toRnaNucleotide = Map.fromList [ ('G','C'), ('C','G'), ('T','A'), ('A','U') ]
 
-toRNA :: String -> Maybe String
-toRNA = mapM (`Map.lookup` toRnaNucleotide)
+toRNA :: (Traversable t) => t Char -> Maybe (t Char)
+toRNA = traverse (`Map.lookup` toRnaNucleotide)
