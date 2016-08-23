2faeb8f1ae03bb41a77c858f915a6643cfbc45fd
Achim <abo64@gmx.de>
2016-08-23 12:37:22 +0200

    use collect

--- java/strain/src/main/java/Strain.java
+++ java/strain/src/main/java/Strain.java
@@ -1,25 +1,19 @@
 import java.util.ArrayList;
 import java.util.List;
-import java.util.function.BiFunction;
-import java.util.function.BinaryOperator;
+import java.util.function.BiConsumer;
 import java.util.function.Predicate;
 
 public class Strain {
 
     public static <T> List<T> keep(List<T> ts, Predicate<T> p) {
         return ts.stream()
-                 .reduce(new ArrayList<>(), addIf(p), first());
+                 .collect(ArrayList::new, addIf(p), List::addAll);
     }
 
-    private static <T> BiFunction<List<T>,T,List<T>> addIf(Predicate<T> p) {
+    private static <T> BiConsumer<List<T>,T> addIf(Predicate<T> p) {
         return (filteredTs, t) -> {
                   if (p.test(t)) filteredTs.add(t);
-                  return filteredTs;
                };
-    }
-
-    private static <T> BinaryOperator<T> first() {
-        return (_1, _2) -> _1;
     }
 
     public static <T> List<T> discard(List<T> ts, Predicate<T> p) {
