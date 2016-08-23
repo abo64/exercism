ec7b256440e951f10e5ec88d1179757aebc96a34
Achim <abo64@gmx.de>
2016-08-23 11:22:54 +0200

    version w/ reduce and boxed

--- java/sieve/src/main/java/Sieve.java
+++ java/sieve/src/main/java/Sieve.java
@@ -1,8 +1,8 @@
 import java.util.ArrayList;
 import java.util.List;
 import java.util.function.BiFunction;
+import java.util.function.BinaryOperator;
 import java.util.function.Predicate;
-import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class Sieve {
@@ -14,12 +14,13 @@ public class Sieve {
     }
 
     public List<Integer> getPrimes() {
-        List<Integer> candidates =
-            IntStream.rangeClosed(2, n)
-             .mapToObj(Integer::new)
-             .collect(Collectors.toList());
+        return IntStream.rangeClosed(2, n)
+                .boxed()
+                .reduce(new ArrayList<Integer>(), addIfPrime, first());
+    }
 
-        return foldLeft(candidates, new ArrayList<Integer>(), addIfPrime);
+    private static <T> BinaryOperator<T> first() {
+        return (_1, _2) -> _1;
     }
 
     private static BiFunction<List<Integer>, Integer, List<Integer>> addIfPrime =
@@ -36,12 +37,4 @@ public class Sieve {
 
             return primes;
         };
-
-    private static <A, B> B foldLeft(Iterable<A> as, B z, BiFunction<B,A,B> f) {
-        B result = z;
-        for (A a : as) {
-            result = f.apply(result, a);
-        }
-        return result;
-    }
 }
