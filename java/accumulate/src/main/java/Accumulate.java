5c641a7cc1198c49bcb023c24d18a1491e4091f2
Achim <abo64@gmx.de>
2016-08-23 12:38:45 +0200

    cosmetics

--- java/accumulate/src/main/java/Accumulate.java
+++ java/accumulate/src/main/java/Accumulate.java
@@ -7,10 +7,10 @@ public class Accumulate {
 
     public static <T> List<T> accumulate(List<T> ts, Function<T, T> f) {
         return ts.stream()
-                 .collect(ArrayList::new, map(f), ArrayList::addAll);
+                 .collect(ArrayList::new, map(f), List::addAll);
     }
 
-    private static <T> BiConsumer<ArrayList<T>,T> map(Function<T, T> f) {
+    private static <T> BiConsumer<List<T>,T> map(Function<T, T> f) {
         return (fts, t) -> {
             fts.add(f.apply(t));
         };
