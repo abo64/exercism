7bbfc21bae17c90ac1255fc41530fc4f546fc3b2
Achim <abo64@gmx.de>
2016-08-23 12:32:17 +0200

    use collect

--- java/accumulate/src/main/java/Accumulate.java
+++ java/accumulate/src/main/java/Accumulate.java
@@ -1,24 +1,18 @@
 import java.util.ArrayList;
 import java.util.List;
-import java.util.function.BiFunction;
-import java.util.function.BinaryOperator;
+import java.util.function.BiConsumer;
 import java.util.function.Function;
 
 public class Accumulate {
 
     public static <T> List<T> accumulate(List<T> ts, Function<T, T> f) {
         return ts.stream()
-                 .reduce(new ArrayList<>(), map(f), first());
+                 .collect(ArrayList::new, map(f), ArrayList::addAll);
     }
 
-    private static <T> BiFunction<List<T>,T,List<T>> map(Function<T, T> f) {
+    private static <T> BiConsumer<ArrayList<T>,T> map(Function<T, T> f) {
         return (fts, t) -> {
             fts.add(f.apply(t));
-            return fts;
         };
-    }
-
-    private static <T> BinaryOperator<T> first() {
-        return (_1, _2) -> _1;
     }
 }
