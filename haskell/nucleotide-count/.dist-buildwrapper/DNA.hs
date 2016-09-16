b0eadb857edc7021107edf0c77ec03458d14cc4c
Achim <abo64@gmx.de>
2016-09-16 07:03:04 +0200

    haskell/nucleotide-count: solution w/ foldM

--- haskell/nucleotide-count/.dist-buildwrapper/DNA.hs
+++ haskell/nucleotide-count/.dist-buildwrapper/DNA.hs
@@ -1,29 +1,23 @@
+{-# LANGUAGE TupleSections #-}
 module DNA (count, nucleotideCounts) where
 
 import Data.List as L
 import Data.Map as M
+import Control.Monad (foldM)
 
 count :: Char -> String -> Either String Int
-count char dna =
-  toNucleotide char >> foldDNA countNucleotide 0 dna
-  where
-    countNucleotide counter nucleotide =
-      counter + fromEnum (nucleotide == char)
+count char dna = do
+  c <- toNucleotide char
+  counts <- nucleotideCounts dna
+  return $ counts ! c
 
 nucleotideCounts :: String -> Either String (Map Char Int)
-nucleotideCounts = foldDNA countNucleotides zeroCounts
+nucleotideCounts = foldM countNucleotides zeroCounts
   where
-    countNucleotides = flip $ M.adjust succ
-    zeroCounts = M.fromList (L.map zeroCount nucleotides)
-    zeroCount nucl = (nucl, 0)
-
-foldDNA :: (a -> Char -> a) -> a -> String -> Either String a
-foldDNA f zero = L.foldl accumulate (Right zero)
-  where
-    accumulate a char = do
-      acc <- a
+    zeroCounts = M.fromList $ L.map (, 0) nucleotides
+    countNucleotides counts char = do
       nucleotide <- toNucleotide char
-      return $ f acc nucleotide
+      return $ M.adjust succ nucleotide counts
 
 toNucleotide :: Char -> Either String Char
 toNucleotide char =
