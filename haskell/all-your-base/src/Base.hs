60436bfbfcf23ffab563b2f70742fb7a2b8b5307
Achim <abo64@gmx.de>
2016-09-19 06:52:57 +0200

    use guard function

--- haskell/all-your-base/src/Base.hs
+++ haskell/all-your-base/src/Base.hs
@@ -1,35 +1,28 @@
 module Base (rebase) where
 
-import Control.Monad (mfilter, foldM)
+import Control.Monad (foldM, guard)
 import Data.Sequence (unfoldl)
 import Data.Foldable (toList)
 
 rebase :: Integral a => a -> a -> [a] -> Maybe [a]
 rebase inputBase outputBase inputDigits = do
-  inBase <- validBase inputBase
-  outBase <- validBase outputBase
-  dec <- toDecimal inBase inputDigits
-  return $ fromDecimal outBase dec
+  guard $ all (>= 2) [inputBase, outputBase]
+  dec <- toNumber inputBase inputDigits
+  return $ fromNumber outputBase dec
 
-validBase :: Integral a => a -> Maybe a
-validBase = mfilter (> 1) . Just
-
-validDigit :: Integral a => a -> a -> Maybe a
-validDigit base = mfilter (\x -> x > -1 && x < base) . Just
-
-toDecimal :: Integral a => a -> [a] -> Maybe a
-toDecimal base = foldM nextInt 0
+toNumber :: Integral a => a -> [a] -> Maybe a
+toNumber base = foldM nextInt 0
   where
-    nextInt acc = fmap (acc*base +) . validDigit base
---toDecimal base = fmap sum . zipWithM validMult powers . reverse
---  where
---    validMult p = fmap (p*) . validDigit base
---    powers = map (base^) [0,1..]
+    nextInt acc digit = do
+      guard $ digit >= 0 && digit < base
+      return $ acc*base + digit
 
-fromDecimal :: Integral a => a -> a -> [a]
-fromDecimal base = toList . unfoldl nextDigit
+fromNumber :: Integral a => a -> a -> [a]
+fromNumber base = toList . unfoldl nextDigit
   where
-    nextDigit = fmap (`quotRem` base) . mfilter (> 0) . Just
+    nextDigit number = do
+      guard $ number > 0
+      return $ quotRem number base
 --    nextDigit dec =
 --      | dec > 0 = Just (quotRem dec base)
 --      | otherwise = Nothing
