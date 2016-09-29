36450d649649b3c331d418bb91946b5b21a2f515
Achim <abo64@gmx.de>
2016-09-29 10:05:37 +0200

    solution for Option return type

--- scala/phone-number/src/main/scala/PhoneNumber.scala
+++ scala/phone-number/src/main/scala/PhoneNumber.scala
@@ -2,36 +2,34 @@ import PhoneNumber._
 
 class PhoneNumber(phoneNumber: String) {
 
-  lazy val (areaCode, prefix, lineNumber) =
-    parse(phoneNumber) getOrElse InvalidPhoneNumberParts
+  lazy val parts: Option[Parts] = parse(phoneNumber)
 
-  lazy val number: String = s"$areaCode$prefix$lineNumber"
+  lazy val number: Option[String] =
+    parts map { case (areaCode, prefix, lineNumber) => s"$areaCode$prefix$lineNumber" }
 
-  override def toString = s"($areaCode) $prefix-$lineNumber"
+  lazy val areaCode: Option[String] = parts map (_._1)
+
+  lazy val prettyPrint: Option[String]  = 
+    parts map { case (areaCode, prefix, lineNumber) => s"($areaCode) $prefix-$lineNumber" }
 }
 
 object PhoneNumber {
-  type AreaCode = String
-  type Prefix = String
-  type LineNumber = String
-  type PhoneNumberParts = (AreaCode, Prefix, LineNumber)
+  type Parts = (String, String, String)
 
   private[this] val AreaCodeLength = 3
   private[this] val PrefixLength = 3
   private[this] val LineNumberLength = 4
-
-  val InvalidPhoneNumberParts: PhoneNumberParts =
-    ("0" * AreaCodeLength, "0" * PrefixLength, "0" * LineNumberLength)
 
   private[this] val PhoneNumberPattern = {
     def part(length: Int) = s"(\\d{$length})"
     s"""1?${part(AreaCodeLength)}${part(PrefixLength)}${part(LineNumberLength)}""".r
   }
 
-  def parse(phoneNumber: String): Option[PhoneNumberParts] = {
+  def parse(phoneNumber: String): Option[Parts] = {
     val digits = phoneNumber filter (_.isDigit)
     digits match {
-      case PhoneNumberPattern(areaCode, prefix, lineNumber) => Some((areaCode, prefix, lineNumber))
+      case PhoneNumberPattern(areaCode, prefix, lineNumber) =>
+        Some((areaCode, prefix, lineNumber))
       case _ => None
     }
   }
