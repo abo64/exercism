29a1957989ac25d541b1d57d9c1c3a6c5f273299
Achim <abo64@gmx.de>
2016-10-10 07:19:18 +0200

    shorter solution inspired by Haskell

--- scala/house/src/main/scala/House.scala
+++ scala/house/src/main/scala/House.scala
@@ -1,56 +1,25 @@
 object House {
-  type Sentence = String
-  type Noun = String
-  type Verb = String
-
   def rhyme: String =
-    sentences(Lexicon.size)
+    versions(lines) map combine mkString("", "\n", "\n")
 
-  private val SentenceSeparator = "\n\n"
-  private val RelativeClauseSeparator = "\n"
+  val combine: Seq[String] => String =
+    ("This is " + _) compose (_.mkString)
 
-  private def sentences(howMany: Int): String = {
-    val sentences: Seq[Sentence] =
-      (0 until howMany) map sentence
-    sentences mkString("", SentenceSeparator, SentenceSeparator)
-  }
+  val versions: Seq[String] => Seq[Seq[String]] =
+    ((_:Seq[String]).tails.toSeq) andThen (_.reverse) andThen (_.tail)
 
-  private def sentence(n: Int): String =
-    "This is" + nounPhrase(n)
-
-  private def nounPhrase(embeddings: Int, text: String = ""): String = {
-    val np = " the " + noun(embeddings)
-    relativeClause(embeddings, text + np)
-  }
-
-  private def relativeClause(embeddings: Int, text: String = "") = {
-    val terminate = embeddings == 0
-
-    val separator = if (terminate) " " else RelativeClauseSeparator
-    val clause = separator + "that " + verb(embeddings)
-    val newText = text + clause
-
-    if (terminate)
-      newText
-    else
-      nounPhrase(embeddings - 1, newText)
-  }
-
-  private def noun(whichOne: Int): Noun = Lexicon(whichOne)._1
-  private def verb(whichOne: Int): Verb = Lexicon(whichOne)._2
-
-  private val Lexicon: Seq[(Noun, Verb)] = Seq(
-    ("horse and the hound and the horn", "belonged to"),
-    ("farmer sowing his corn", "kept"),
-    ("rooster that crowed in the morn", "woke"),
-    ("priest all shaven and shorn", "married"),
-    ("man all tattered and torn", "kissed"),
-    ("maiden all forlorn", "milked"),
-    ("cow with the crumpled horn", "tossed"),
-    ("dog", "worried"),
-    ("cat", "killed"),
-    ("rat", "ate"),
-    ("malt", "lay in"),
-    ("house", "Jack built.")
-  ).reverse
+  val lines = Seq(
+                "the horse and the hound and the horn\nthat belonged to ",
+                "the farmer sowing his corn\nthat kept ",
+                "the rooster that crowed in the morn\nthat woke ",
+                "the priest all shaven and shorn\nthat married ",
+                "the man all tattered and torn\nthat kissed ",
+                "the maiden all forlorn\nthat milked ",
+                "the cow with the crumpled horn\nthat tossed ",
+                "the dog\nthat worried ",
+                "the cat\nthat killed " ,
+                "the rat\nthat ate ",
+                "the malt\nthat lay in " ,
+                "the house that Jack built.\n"
+              )
 }
