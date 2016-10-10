b83af20e9768dcf28a1cfbd4121dacae1ae5acb4
Achim <abo64@gmx.de>
2016-10-10 07:10:53 +0200

    solution w/ zipWithExactMay

--- scala/hamming/src/main/scala/Hamming.scala
+++ scala/hamming/src/main/scala/Hamming.scala
@@ -1,17 +1,20 @@
 import scala.annotation.tailrec
 
 object Hamming {
-  def compute[T](xs: Seq[T], ys: Seq[T]): Option[Int] = {
-    def difference(x: T, y: T): Int = if (x != y) 1 else 0
+  def compute[T](xs: Seq[T], ys: Seq[T]): Option[Int] =
+    zipWithExactMay(xs, ys)(_ != _) map (_.count(identity))
+    // Haskell: genericLength . filter id <$> zipWithExactMay (/=) xs ys
+    // (Haskell doesn't have a "count" function)
 
+  private def zipWithExactMay[A,B,C](as: Seq[A], bs: Seq[B])(f: (A,B) => C): Option[Seq[C]] = {
     @tailrec
-    def loop(xs: Seq[T], ys: Seq[T], acc: Int): Option[Int] =
-      (xs, ys) match {
-        case (Nil, Nil) => Some(acc)
-        case (a+:as, b+:bs) => loop(as, bs, acc + difference(a, b))
+    def loop(as: Seq[A], bs: Seq[B], cs: Seq[C]): Option[Seq[C]] =
+      (as, bs) match {
+        case (Nil, Nil) => Some(cs)
+        case (a+:as, b+:bs) => loop(as, bs, cs :+ f(a, b))
         case _ => None
       }
 
-    loop(xs, ys, 0)
+    loop(as, bs, Seq())
   }
 }
