23df866c1759da0f10537c9a883d9a759e0c1e0f
Achim <abo64@gmx.de>
2016-10-11 09:49:15 +0200

    solution w/ scalaz traverse

--- scala/rna-transcription/build.sbt
+++ scala/rna-transcription/build.sbt
@@ -1,3 +1,5 @@
 scalaVersion := "2.11.7"
 
 libraryDependencies += "org.scalatest" % "scalatest_2.11" % "2.2.5" % "test"
+
+libraryDependencies += "org.scalaz" %% "scalaz-core" % "7.2.6"
--- scala/rna-transcription/src/main/scala/Dna.scala
+++ scala/rna-transcription/src/main/scala/Dna.scala
@@ -1,16 +1,19 @@
-import Dna._
+import scalaz.std.list._
+import scalaz.std.option._
+import scalaz.syntax.traverse._
 
 object Dna {
   private type Strand = String
   private type Nucleotide = Char
 
-  def toRna(dna: Strand): Option[Strand] =
-    dna.foldRight (Option("")) { case (char, rna) =>
-      for {
-        rnaNucleotide <- toRnaNucleotide(char)
-        rnaStrand <- rna
-      } yield rnaNucleotide +: rnaStrand
-  }
+  def toRna(dna: String): Option[Strand] =
+    dna.toList.traverse(toRnaNucleotide) map (_.mkString)
+//    dna.foldRight (Option("")) { case (char, rna) =>
+//      for {
+//        rnaNucleotide <- toRnaNucleotide(char)
+//        rnaStrand <- rna
+//      } yield rnaNucleotide +: rnaStrand
+//  }
 
   private val DnaToRna =
     Map('G' -> 'C', 'C' -> 'G', 'T' -> 'A', 'A' -> 'U')
