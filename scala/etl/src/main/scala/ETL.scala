8e471bafd693389719078a29bfc121246e2d5e19
Achim <abo64@gmx.de>
2016-10-12 07:16:39 +0200

    solution w/ foldLeft

--- scala/etl/src/main/scala/ETL.scala
+++ scala/etl/src/main/scala/ETL.scala
@@ -2,15 +2,12 @@ object ETL {
   type Letter = String
   type Point = Int
   type Letters = Seq[Letter]
-  type OldScore = (Point,Letters)
-  type NewScore = (Letter,Point)
   type OldScoreFormat = Map[Point,Letters]
   type NewScoreFormat = Map[Letter,Point]
 
   def transform(oldScoreFormat: OldScoreFormat): NewScoreFormat =
-    for {
-      (point, letters) <- oldScoreFormat
-      letter <- letters
-      newScore = (letter.toLowerCase, point)
-    } yield newScore
+    oldScoreFormat.foldLeft(Map.empty[Letter,Point]) {
+      case (result, (point, letters)) =>
+        result ++ (letters map (letter => (letter.toLowerCase, point)))
+      }
 }
