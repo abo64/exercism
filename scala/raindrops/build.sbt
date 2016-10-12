7d4fee84fc5f4e0a47964eddd62da8d96705e7b0
Achim <abo64@gmx.de>
2016-10-12 07:24:23 +0200

    solution w/ scalaz Foldable

--- scala/raindrops/build.sbt
+++ scala/raindrops/build.sbt
@@ -1,3 +1,8 @@
 scalaVersion := "2.11.7"
 
 libraryDependencies += "org.scalatest" % "scalatest_2.11" % "2.2.5" % "test"
+
+libraryDependencies += "org.scalaz" %% "scalaz-core" % "7.2.6"
+
+libraryDependencies += "org.typelevel" %% "cats" % "0.7.2"
+
--- scala/raindrops/src/main/scala/Raindrops.scala
+++ scala/raindrops/src/main/scala/Raindrops.scala
@@ -1,34 +1,31 @@
+import scalaz.std.list._
+import scalaz.std.option._
+import scalaz.std.string._
+import scalaz.syntax.traverse._
+
 object Raindrops {
 
   // needed for the tests to compile and run
   def apply() = this
 
   private val factorMessages =
-    Seq((3, "Pling"),
-        (5, "Plang"),
-        (7, "Plong"))
+    List((3, "Pling"),
+         (5, "Plang"),
+         (7, "Plong"))
 
   def convert(number: Int): String = {
     def hasFactor(factor: Int): Boolean =
       number % factor == 0
 
-    import Monad._
-
     val getMessage: ((Int, String)) => Option[String] = {
       case (factor, message) =>
-        (Option(factor) guard hasFactor(factor)) >> unit(message)
-//        Option(factor) filter hasFactor map const(message)
-//        for {
-//          _ <- Option(factor) if hasFactor(factor)
-//        } yield message
+        Option(factor) filter hasFactor map const(message)
     }
     // Haskell: getMessage (f, xs) = guard (x `mod` f == 0) >> return xs
-
-    import Foldable._
 
     factorMessages foldMap getMessage getOrElse number.toString
     // Haskell: fromMaybe (show x) (foldMap getMessage factorMessages)
   }
 
-//  private def const[A,B](a: A)(ignore: B): A = a
+  private def const[A,B](a: A)(ignore: B): A = a
 }
