c668e187135120490a8e13f54c9a7ed246b2a0f3
Achim <abo64@gmx.de>
2016-12-12 07:31:38 +0100

    scala/parallel-letter-frequency: sequential and par solution added

--- scala/parallel-letter-frequency/src/main/scala/Frequency.scala
+++ scala/parallel-letter-frequency/src/main/scala/Frequency.scala
@@ -1,17 +1,50 @@
 import java.util.concurrent.Executors
-import concurrent.{ExecutionContext, Await, Future}
-import concurrent.duration.DurationInt
+
+import scala.collection.parallel.ForkJoinTaskSupport
+import scala.collection.parallel.ParSeq
+import scala.concurrent.{ExecutionContext, Await, Future}
+import scala.concurrent.duration.DurationInt
+import scala.concurrent.forkjoin.ForkJoinPool
 
 object Frequency {
   type CharFrequency = Map[Char,Int]
 
   def frequency(threads: Int, text: Seq[String]): CharFrequency = {
+//    seqFrequency(text)
+    parFrequency(threads, text)
+//    futureFrequency(threads, text)
+  }
+
+  // sequential solution
+  private def seqFrequency(text: Seq[String]): CharFrequency = {
+    val lineFrequencies: Seq[CharFrequency] =
+      text map charFrequency
+    lineFrequencies.fold(Map[Char,Int]())(addFrequencies)
+  }
+
+  // using parallel collections
+  private def parFrequency(threads: Int, text: Seq[String]): CharFrequency = {
+    val parText: ParSeq[String] =
+      kestrel (text.par) {
+        _.tasksupport = new ForkJoinTaskSupport(new ForkJoinPool(threads))
+      }
+
+    val lineFrequencies: ParSeq[CharFrequency] =
+      parText map charFrequency
+    lineFrequencies.fold(Map[Char,Int]())(addFrequencies)
+  }
+
+  // using scala.concurrent.Future
+  private def futureFrequency(threads: Int, text: Seq[String]): CharFrequency = {
     implicit val executionContext =
       ExecutionContext.fromExecutor(Executors.newWorkStealingPool(threads))
 
-    val frequencyFutures = text map charFrequencyAsync
-    val futureFrequencies = Future.fold(frequencyFutures)(EmptyCharFrequency)(addFrequencies)
-    Await.result(futureFrequencies, 5 seconds)
+    val frequencyFutures: Seq[Future[CharFrequency]] =
+      text map charFrequencyAsync
+    val futureFrequencies: Future[CharFrequency] = 
+      Future.fold(frequencyFutures)(EmptyCharFrequency)(addFrequencies)
+
+      Await.result(futureFrequencies, 5 seconds)
   }
 
   private def charFrequencyAsync(line: String)(implicit executor: ExecutionContext): Future[CharFrequency] =
@@ -33,4 +66,7 @@ object Frequency {
 
   private def addFrequencies(map1: CharFrequency, map2: CharFrequency) =
     map1 ++ map2.map{ case (k,v) => k -> (v + map1.getOrElse(k,0)) }
+
+  // kestrel combinator for side effect
+  private def kestrel[A](a: A)(f: A => Unit): A = { f(a); a }
 }
