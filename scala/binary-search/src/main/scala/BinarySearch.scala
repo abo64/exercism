d149835e37426c00d33d887438029f7acc6e2c0d
Achim <abo64@gmx.de>
2017-02-28 06:58:39 +0100

    solution for scala/binary-search

---
+++ scala/binary-search/src/main/scala/BinarySearch.scala
@@ -0,0 +1,19 @@
+import scala.annotation.tailrec
+
+object BinarySearch {
+  def search[T](ts: Seq[T], what: T)(implicit ev: T => Ordered[T]): Option[Int] =
+  {
+    @tailrec
+    def loop(start: Int, end: Int): Option[Int] =
+        if (start < 0 || end <= start) None
+        else {
+          val pivot = (end + start) / 2
+          val found = ts(pivot)
+          if (found == what) Some(pivot)
+          else if (found > what) loop(start, pivot)
+          else loop(pivot + 1, end)
+        }
+
+    loop(0, ts.length)
+  }
+}
