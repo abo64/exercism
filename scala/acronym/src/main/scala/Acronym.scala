3a5b2eaca6cf13c286cd17c319681bf9e9147409
Achim <abo64@gmx.de>
2017-04-27 15:07:05 +0200

    solution for scala/acronym with zip

--- scala/acronym/src/main/scala/Acronym.scala
+++ scala/acronym/src/main/scala/Acronym.scala
@@ -1,35 +1,13 @@
-import Acronym._
-
-class Acronym(phrase: String) {
-
-  def abbreviate: Abbreviation = {
-    val sanitizedPhrase = phrase filterNot noise
-    val words = splitWords(sanitizedPhrase)
-    val upperFirstWordChars = words map (_.head.toUpper)
-    upperFirstWordChars mkString
-  }
-}
-
 object Acronym {
-  type Phrase = String
-  type Word = String
-  type Abbreviation = String
 
-  def apply(phrase: Phrase): Acronym = new Acronym(phrase)
+  def abbreviate(phrase: String): String = {
+    def acronymChar(x: Char, y: Char): Option[Char] =
+      if (!x.isLetter && y.isLetter) Some(y.toUpper)
+      else if (!x.isUpper && y.isUpper) Some(y)
+      else None
 
-  private val WordSeparators = " -"
+    val charPairs = ' ' +: phrase zip phrase
 
-  private def noise(char: Char): Boolean =
-    !char.isLetter && !WordSeparators.contains(char)
-
-  private def splitWords(phrase: Phrase): Seq[Word] = {
-    def splitCamelCaseWords(word: Word): Seq[Word] = {
-      // stolen from
-      // http://stackoverflow.com/questions/7593969/regex-to-split-camelcase-or-titlecase-advanced
-      word split "(?<!(^|[A-Z]))(?=[A-Z])|(?<!^)(?=[A-Z][a-z])"
-    }
-
-    val words = phrase split s"[$WordSeparators]"
-    words flatMap splitCamelCaseWords
+    charPairs flatMap (acronymChar _).tupled.andThen(_.toList) mkString
   }
 }
