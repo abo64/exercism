3000d94c3c30bdccf9ad5ca1de5ed7aa50c04d79
abo64 <abo64@gmx.de>
2018-05-03 00:15:36 +0200

    Add collatz-conjecture solution using a Stream

--- scala/collatz-conjecture/build.sbt
+++ scala/collatz-conjecture/build.sbt
@@ -1,3 +1,3 @@
 scalaVersion := "2.12.5"
 
-libraryDependencies += "org.scalatest" %% "scalatest" "3.0.5" % "test"
+libraryDependencies += "org.scalatest" %% "scalatest" % "3.0.5" % "test"
--- scala/collatz-conjecture/src/main/scala/CollatzConjecture.scala
+++ scala/collatz-conjecture/src/main/scala/CollatzConjecture.scala
@@ -1,17 +1,36 @@
+import scala.annotation.tailrec
+import scala.language.postfixOps
+
 object CollatzConjecture {
 
   def steps(n: Int): Option[Int] = {
+    Option(n) filter (_ > 0) map collatzStream
 
-    def even(i: Int): Boolean = {
-      i % 2 == 0
+//    Option(n) filter (_ > 0) map collatzRec(0)
+  }
+
+    @tailrec
+    def collatzRec(step: Int)(m: Int): Int = {
+      m match {
+        case 1 => step
+        case x if even(x) => collatzRec(step + 1)(x / 2)
+        case x => collatzRec(step + 1)(x * 3 + 1)
+      }
     }
 
-    def collatz(step: Int)(m: Int): Int = m match {
-      case 1 => step
-      case x if even(x) => collatz(step + 1)(x / 2)
-      case x => collatz(step + 1)(x * 3 + 1)
-    }
 
-    Option(n) filter (_ > 0) map collatz(0)
+  private def collatzStream(n: Int): Int = {
+
+    val collatzNumbers: Stream[Int] =
+      Stream.iterate(n) {
+          case x if even(x) => x / 2
+          case x => x * 3 + 1
+      }
+
+    collatzNumbers takeWhile (_ != 1) size
+  }
+
+  private def even(i: Int): Boolean = {
+    i % 2 == 0
   }
 }
